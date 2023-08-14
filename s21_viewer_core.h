f9b9abf187f74f929377af1a40aa0d1485e02864
Bober <thedillerthe@gmail.com>
2023-08-14 15:24:55 +0300

    comment

--- s21_viewer_core.h
+++ s21_viewer_core.h
@@ -6,6 +6,9 @@
 #include "vertex.h"
 
 void move(LinkedListVertex* list, double x_move, double y_move, double z_move);
+
+// axis : 'X' / 'Y' / 'Z'
+// route: 'R' / 'L'
 void rotate(LinkedListVertex* list, double angle, char axis, char route);
 void scale(LinkedListVertex* list, double x_sc, double y_sc, double z_sc);
 
