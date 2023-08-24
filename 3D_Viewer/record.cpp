b15c3f6818cd5d17194b4f15ee52c4c491e32508
Bober <thedillerthe@gmail.com>
2023-08-24 16:02:35 +0300

    dialog screen

--- 3D_Viewer/record.cpp
+++ 3D_Viewer/record.cpp
@@ -4,19 +4,18 @@ Record::Record(QObject* parent) : QObject{parent} {}
 
 void Record::slotMakeScreen() {
     QImage image = glView->grabFrameBuffer();
-    QString fileName;
-    fileName += "3D_Viewer_Screen_";
-    fileName += QDate::currentDate().toString("yyyy-MM-dd");
-    fileName += "_";
-    fileName += QTime::currentTime().toString("hh:mm:ss");
 
     if (qobject_cast<QAction*>(sender()) == recordMenu->actions().at(0)) {
-        fileName += ".jpeg";
+        QString fileName = QFileDialog::getSaveFileName(
+            qobject_cast<QWidget*>(this), "Save as...", "screen.jpeg",
+            "JPEG (*.jpeg)");
         image.save(fileName, "JPEG", 85);
         qDebug() << fileName;
     } else if (qobject_cast<QAction*>(sender()) ==
                recordMenu->actions().at(1)) {
-        fileName += ".bmp";
+        QString fileName = QFileDialog::getSaveFileName(
+            qobject_cast<QWidget*>(this), "Save as...", "screen.bmp",
+            "BMP (*.bmp)");
         image.save(fileName, "BMP");
         qDebug() << fileName;
     }
@@ -33,16 +32,13 @@ void Record::slotStartTimer() {
 }
 
 void Record::slotMakeGif() {
-    QString fileName;
-    fileName += "3D_Viewer_Screen_";
-    fileName += QDate::currentDate().toString("yyyy-MM-dd");
-    fileName += "_";
-    fileName += QTime::currentTime().toString("hh:mm:ss");
-    fileName += ".gif";
     QImage image = glView->grabFrameBuffer().scaled(gifWidth, gifHeight);
     gif->addFrame(image);
     timerCounter++;
     if (timerCounter == 50) {
+        QString fileName = QFileDialog::getSaveFileName(
+            qobject_cast<QWidget*>(this), "Save as...", "animation.gif",
+            "GIF (*.gif)");
         timerCounter = 0;
         timer->stop();
         gif->save(fileName);
